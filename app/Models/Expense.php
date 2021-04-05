1ed4ee81bc604072965dbb24c624a53e1bd1fd1f
diaahassouna <sightmare@gmail.com>
2021-04-05 23:31:54 +0200

    first commit

---
+++ app/Models/Expense.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    use HasFactory;
+    public $fillable = [
+        'monthly_income',
+        'daily_income',
+        'fixed_expenses',
+        'variable_expenses',
+        'non_necessities_expenses',
+        'saving_plan_percentage',
+        'balance',
+    ];
+}
---
+++ database/migrations/2021_04_05_184158_create_expenses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->float('monthly_income');
+            $table->float('daily_income');
+            $table->float('fixed_expenses');
+            $table->float('variable_expenses');
+            $table->float('non_necessities_expenses');
+            $table->float('saving_plan_percentage');
+            $table->float('balance');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
